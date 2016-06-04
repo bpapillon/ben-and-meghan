c0306bcea7db1895edb023351709c92973069910
Ben Papillon <bpapillon@umbel.com>
2016-06-03 19:51:54 -0500

    cms changes

--- wedding/admin.py
+++ wedding/admin.py
@@ -4,8 +4,9 @@ from .models import Rsvp
 
 
 class RsvpAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'rsvp_code', 'responded', 'attending')
+    list_display = ('name', 'email', 'rsvp_code', 'responded', 'attending', 'updated_date')
     search_fields = ('name', 'rsvp_code', 'rsvp_code_slug',)
+    ordering = ('-updated_date',)
 
 
 admin.site.register(Rsvp, RsvpAdmin)
