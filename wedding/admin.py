b0b314d3ed80d95efa42d44b1582347bd2ffce9b
Ben Papillon <bpapillon@umbel.com>
2016-08-01 21:04:57 -0400

    admin improvements

--- wedding/admin.py
+++ wedding/admin.py
@@ -4,9 +4,10 @@ from .models import Rsvp
 
 
 class RsvpAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'rsvp_code', 'responded', 'attending', 'updated_date')
-    search_fields = ('name', 'rsvp_code', 'rsvp_code_slug',)
+    list_display = ('name', 'email', 'rsvp_code', 'responded', 'attending', 'confirmed_party_size', 'updated_date')
+    list_filter = ('attending', 'responded',)
     ordering = ('-updated_date',)
+    search_fields = ('name', 'rsvp_code', 'rsvp_code_slug',)
 
 
 admin.site.register(Rsvp, RsvpAdmin)
